042c6ac8ee266694b30cc656cde339fde851e2b8
LeTryuk <tarakan773@gmail.com>
2024-10-14 14:51:46 +0700

    Initial Commit

---
+++ shiftlab-2024/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  shiftlab-2024
+//
+//  Created by Тимур Осокин on 14.10.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
